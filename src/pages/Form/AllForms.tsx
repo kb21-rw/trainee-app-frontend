dc5656048dd068688758dcdef140fbe424896f14
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-21 12:51:59 +0200

    refactor: update all forms page

--- src/pages/Form/AllForms.tsx
+++ src/pages/Form/AllForms.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import SearchInput from "../../components/ui/SearchInput";
 import {
   useGetAllFormsQuery,
-  useGetApplicationFormQuery
+  useGetApplicationFormQuery,
 } from "../../features/user/apiSlice";
 import FormCard from "../../components/ui/FormCard";
 import { IFormType } from "../../utils/types";
@@ -10,6 +10,7 @@ import Loader from "../../components/ui/Loader";
 import NotFound from "../../components/ui/NotFound";
 import { getJWT } from "../../utils/helper";
 import CreateFormDropdown from "../../components/ui/CreateFormDropdown";
+import { Link } from "react-router-dom";
 
 const AllForms = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -18,7 +19,7 @@ const AllForms = () => {
     jwt,
     searchString: searchQuery,
   });
-  const {data: applicationForm} = useGetApplicationFormQuery(jwt);
+  const { data: applicationForm } = useGetApplicationFormQuery(jwt);
 
   const parsedApplicationForm = {
     _id: "6707a1241caed33cd7c941dc",
@@ -26,17 +27,14 @@ const AllForms = () => {
     description: applicationForm?.description,
     type: applicationForm?.type,
     questions: applicationForm?.questions.length,
-    startDate: applicationForm?.startDate
-  }
+    startDate: applicationForm?.startDate,
+  };
   const forms = data?.forms;
-
-
-  
 
   return (
     <div className="py-12">
       <div className="flex justify-between items-center my-5">
-        <SearchInput setSearchQuery={setSearchQuery} />  
+        <SearchInput setSearchQuery={setSearchQuery} />
         <CreateFormDropdown />
       </div>
       {isFetching ? (
@@ -49,7 +47,14 @@ const AllForms = () => {
         </div>
       ) : (
         <div className="flex flex-col gap-4 p-4 md:container mx-auto w-3/5 h-[750px] overflow-scroll">
-          {parsedApplicationForm && <FormCard form={parsedApplicationForm} />}
+          {applicationForm && parsedApplicationForm ? (
+            <FormCard form={parsedApplicationForm} />
+          ) : (
+            <div className="flex items-center space-x-1 text-lg rounded-md custom-shadow bg-white p-2">
+              <span>Create a new</span>
+              <Link to="/forms/create/application-form" className="text-primary-dark">application form</Link>
+            </div>
+          )}
           {forms?.map((form: IFormType, index: number) => (
             <FormCard form={form} key={index} />
           ))}
