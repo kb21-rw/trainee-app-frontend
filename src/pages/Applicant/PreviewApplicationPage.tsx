e2ca74fc76ef81128874655c2773ae731d2ba208
David Gusenga <gsngdavid@gmail.com>
2024-11-08 14:36:46 +0200

    feat: make preview page responsive

--- src/pages/Applicant/PreviewApplicationPage.tsx
+++ src/pages/Applicant/PreviewApplicationPage.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAddApplicantResponseMutation } from "../../features/user/backendApi";
-import { getErrorInfo } from "../../utils/helper";
+import { getErrorInfo, getFormattedDate } from "../../utils/helper";
 import { AlertType, Cookie, Question, QuestionType } from "../../utils/types";
 import Button from "../../components/ui/Button";
 import {
@@ -21,7 +21,7 @@ const PreviewApplicationPage = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-  const { formQuestions, responses, formData, formTitle } =
+  const { formQuestions, responses, formData, formTitle, formDeadline } =
     location.state || {};
   const [addApplicantResponse, { isSuccess, error }] =
     useAddApplicantResponseMutation();
@@ -68,76 +68,78 @@ const PreviewApplicationPage = () => {
   }
 
   return (
-    <div className="md:flex items-center justify-center py-5">
-      <div className="flex flex-col items-center justify-center p-5 custom-shadow border-t-[#673AB7] border-t-8 rounded-xl w-full md:w-2/5">
-        <div className="p-4">
-          <h1 className="font-bold text-xl">{formTitle}</h1>
-        </div>
-        <div className="mb-8 p-4 rounded-t-xl md:w-4/5 w-full">
-          {formQuestions?.map((question: Question, index: number) => (
-            <div key={index} className="mb-6">
-              <h3 className="text-md font-semibold text-gray-600 mb-2">
-                {index + 1}. {question.prompt}
-              </h3>
-              {question.type === QuestionType.Text && (
-                <TextField
-                  fullWidth
-                  variant="standard"
-                  value={responses[index]?.answer}
-                  disabled
-                />
-              )}
-              {question.type === QuestionType.SingleSelect && (
-                <RadioGroup
-                  value={responses[index]?.answer}
-                  name={`question-${index}`}
-                >
-                  {question.options?.map(
-                    (option: string, optionIndex: number) => (
-                      <FormControlLabel
-                        key={optionIndex}
-                        value={option}
-                        control={<Radio disabled />}
-                        label={option}
-                        disabled
-                      />
-                    )
-                  )}
-                </RadioGroup>
-              )}
-              {question.type === QuestionType.MultiSelect && (
-                <FormGroup>
-                  {question.options?.map(
-                    (option: string, optionIndex: number) => (
-                      <FormControlLabel
-                        key={optionIndex}
-                        control={
-                          <Checkbox
-                            checked={responses[index]?.answer.includes(option)}
-                            disabled
-                          />
-                        }
-                        label={option}
-                        disabled
-                      />
-                    )
-                  )}
-                </FormGroup>
-              )}
-            </div>
-          ))}
-        </div>
+    <div className="my-12 items-center justify-center flex flex-col p-5 custom-shadow border-t-primary-dark border-t-8 rounded-xl">
+      <div className="p-4 text-center">
+        <h1 className="font-bold text-xl sm:text-4xl">{formTitle}</h1>
+        <p className="text-xs sm:text-base">
+          Application deadline:{" "}
+          <span className="font-bold">{getFormattedDate(formDeadline)}</span>
+        </p>
+      </div>
+      <div className="mb-8 p-4 rounded-t-xl md:w-4/5 w-full">
+        {formQuestions?.map((question: Question, index: number) => (
+          <div key={index} className="mb-6">
+            <h3 className="text-md font-semibold text-gray-600 mb-2">
+              {index + 1}. {question.prompt}
+            </h3>
+            {question.type === QuestionType.Text && (
+              <TextField
+                fullWidth
+                variant="standard"
+                value={responses[index]?.answer}
+                disabled
+              />
+            )}
+            {question.type === QuestionType.SingleSelect && (
+              <RadioGroup
+                value={responses[index]?.answer}
+                name={`question-${index}`}
+              >
+                {question.options?.map(
+                  (option: string, optionIndex: number) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      value={option}
+                      control={<Radio disabled />}
+                      label={option}
+                      disabled
+                    />
+                  )
+                )}
+              </RadioGroup>
+            )}
+            {question.type === QuestionType.MultiSelect && (
+              <FormGroup>
+                {question.options?.map(
+                  (option: string, optionIndex: number) => (
+                    <FormControlLabel
+                      key={optionIndex}
+                      control={
+                        <Checkbox
+                          checked={responses[index]?.answer.includes(option)}
+                          disabled
+                        />
+                      }
+                      label={option}
+                      disabled
+                    />
+                  )
+                )}
+              </FormGroup>
+            )}
+          </div>
+        ))}
+      </div>
 
-        <div className="flex justify-end space-x-4">
-          <Button outlined onClick={backToEdit}>
-            <span className="flex items-center space-x-2">Back to edit</span>
-          </Button>
-          <Button onClick={handleConfirm}>
-            <span className="flex items-center space-x-2">
-              Submit Application
-            </span>
-          </Button>
-        </div>
+      <div className="flex justify-end space-x-4">
+        <Button outlined onClick={backToEdit}>
+          <span className="flex items-center space-x-2">Back to edit</span>
+        </Button>
+        <Button onClick={handleConfirm}>
+          <span className="flex items-center space-x-2">
+            Submit Application
+          </span>
+        </Button>
       </div>
     </div>
   );
