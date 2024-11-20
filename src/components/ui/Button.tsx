1997c12c007a38bc47301fe966a6fe405212f8ff
David Gusenga <gsngdavid@gmail.com>
2024-11-20 15:47:09 +0200

    use classnames in button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,40 +1,12 @@
-import React, { ButtonHTMLAttributes, ReactNode } from 'react'
-import { ButtonSize, ButtonVariant } from '../../utils/types'
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonSize, ButtonVariant } from "../../utils/types";
+import classNames from "classnames";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: ReactNode
-  variant?: ButtonVariant
-  outlined?: boolean
-  size?: ButtonSize
-}
-
-const getVariantOutlinedStyles = (
-  variant: ButtonVariant,
-  outlined: boolean
-) => {
-  switch (variant) {
-    case ButtonVariant.Danger:
-      return outlined
-        ? 'text-red-500 bg-white border-red-500 border px-6'
-        : 'bg-red-500 text-white px-6'
-
-    default:
-      return outlined
-        ? 'text-primary-dark bg-white border-primary-dark border px-6'
-        : 'bg-primary-dark text-white'
-  }
-}
-
-const getSizeStyles = (size: ButtonSize) => {
-  switch (size) {
-    case ButtonSize.Small:
-      return 'px-4 py-1'
-    case ButtonSize.Large:
-      return 'px-7 py-3 text-xl w-full'
-
-    default:
-      return 'px-4 py-2'
-  }
+  children: ReactNode;
+  variant?: ButtonVariant;
+  outlined?: boolean;
+  size?: ButtonSize;
 }
 
 const Button = ({
@@ -42,23 +14,44 @@ const Button = ({
   variant = ButtonVariant.Primary,
   outlined = false,
   size = ButtonSize.Medium,
-  type = 'button',
+  type = "button",
   disabled,
   onClick,
 }: ButtonProps) => {
-  const sizeStyles = getSizeStyles(size)
-  const valiantOutlinedStyles = getVariantOutlinedStyles(variant, outlined)
+  const dangerClasses = {
+    "text-red-500 bg-white border-red-500 border px-6":
+      variant === ButtonVariant.Danger && outlined,
+    "bg-red-500 text-white px-6": variant === ButtonVariant.Danger && !outlined,
+  };
+
+  const primaryClasses = {
+    "text-primary-dark bg-white border-primary-dark border px-6":
+      variant === ButtonVariant.Primary && outlined,
+    "bg-primary-dark text-white":
+      variant === ButtonVariant.Primary && !outlined,
+  };
+
+  const sizeClasses = {
+    "px-4 py-1": size === ButtonSize.Small,
+    "px-4 py-2": size === ButtonSize.Medium,
+    "px-7 py-3 text-xl w-full": size === ButtonSize.Large,
+  };
 
   return (
     <button
       onClick={onClick}
-      className={`rounded-lg ${valiantOutlinedStyles} ${sizeStyles}`}
+      className={classNames(
+        "rounded-lg",
+        dangerClasses,
+        primaryClasses,
+        sizeClasses,
+      )}
       type={type}
       disabled={disabled}
     >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
