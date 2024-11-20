e95ecc6f77af6bdfce66db5947c0b8446e8d032e
David Gusenga <gsngdavid@gmail.com>
2024-11-20 15:24:05 +0200

    refactor: use classnames

--- src/components/ui/Alert.tsx
+++ src/components/ui/Alert.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { hideAlert } from "../../features/user/alertSlice";
 import { AlertType } from "../../utils/types";
+import classNames from "classnames";
 
 const Alert = () => {
   const alert = useSelector((stage: RootState) => stage.alert);
@@ -18,9 +19,11 @@ const Alert = () => {
   return (
     alert.isVisible && (
       <div
-        className={`w-alert-width py-2 flex justify-center items-center rounded-lg absolute top-14 left-1/2 -translate-x-1/2 z-50 border-green-600 ${
-          alert.type === AlertType.Error && "bg-error-light text-error-dark"
-        } ${alert.type === AlertType.Success && "bg-green-300 text-white"}`}
+        className={classNames(
+          `w-alert-width py-2 flex justify-center items-center rounded-lg absolute top-14 left-1/2 -translate-x-1/2 z-50 border-green-600`,
+          { "bg-error-light text-error-dark": alert.type === AlertType.Error },
+          { "bg-green-300 text-white": alert.type === AlertType.Success },
+        )}
       >
         {alert.message}
       </div>
