deb2430d9aad60ec8d4c4859a935ea6e02874d3c
David Gusenga <gsngdavid@gmail.com>
2024-11-20 15:13:21 +0200

    refactor: remove unnecessary classes

--- src/pages/User/Profile.tsx
+++ src/pages/User/Profile.tsx
@@ -51,8 +51,8 @@ const Profile = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-white p-10 rounded-xl custom-shadow">
+    <div className="h-full flex items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md space-y-8 bg-white p-10 rounded-xl custom-shadow">
         <div>
           <H1>Profile Settings</H1>
         </div>
