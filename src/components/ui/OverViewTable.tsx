fa700c96a2a049a03d3e6764d33e3a1639c208fd
David Gusenga <gsngdavid@gmail.com>
2024-11-22 14:58:35 +0200

    refactor: overview

--- src/components/ui/OverViewTable.tsx
+++ src/components/ui/OverViewTable.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 
 interface DataGridProps {
   data: any[];
 }
 
 const OverViewTable: React.FC<DataGridProps> = ({ data }) => {
-  const formatCellValue = (value: any) =>
-    Array.isArray(value) ? value.join(", ") : value;
+  const formatCellValue = (value: any) => {
+    if (!value) return "No response";
+  };
 
   const questionColumns = data[0].forms.flatMap((form: any) =>
     form.questions.map((question: any) => ({
@@ -20,35 +21,54 @@ const OverViewTable: React.FC<DataGridProps> = ({ data }) => {
 
   const formattedColumns: GridColDef[] = [
     { field: "name", headerName: "Name", width: 200 },
-    { field: "coach", headerName: "Coach", width: 200 },
+    {
+      field: "coach",
+      headerName: "Coach",
+      width: 200,
+      valueGetter: (value) => value ?? "No coach",
+    },
     { field: "stage", headerName: "Stage", width: 200 },
     ...questionColumns,
   ];
 
-  const rows = data.flatMap((cohort: any) =>
-    cohort.applicants.map((applicant: any) => {
-      const rowData: { [key: string]: any } = {
-        id: applicant.id,
-        applicantId: applicant.id,
-        stage:
-          applicant.passedStages.length > 0
-            ? applicant.passedStages[0]
-            : "Application",
-        coach: cohort.coaches.length > 0 ? cohort.coaches[0] : "None",
-      };
-
-      cohort.forms.forEach((form: any) => {
-        form.questions.forEach((question: any, qIndex: number) => {
-          const response = question.responses.find(
-            (r: any) => r.user._id === applicant.id,
-          )?.value;
-          rowData[`form_${form._id}_question_${question._id}_${qIndex}`] =
-            response || "No Response yet";
-        });
-      });
-      return rowData;
-    }),
+  const allResponses = data[0].forms.flatMap((form: any) =>
+    form.questions.flatMap((question: any) => question.responses),
   );
+
+  const users = allResponses.reduce((uniqueUsers: any, response: any) => {
+    const userId = response.user._id;
+    if (userId in uniqueUsers) {
+      const existingUser = uniqueUsers[userId];
+
+      return {
+        ...uniqueUsers,
+        [userId]: {
+          ...existingUser,
+          responses: {
+            ...existingUser.responses,
+            [response.questionId]: response.value,
+          },
+        },
+      };
+    }
+
+    return {
+      ...uniqueUsers,
+      [response.user._id]: {
+        user: response.user,
+        responses: { [response.questionId]: response.value },
+      },
+    };
+  }, {});
+
+  const rows: GridRowsProp[] = Object.values(users).map((user: any) => {
+    return {
+      id: user.user._id,
+      name: user.user.name,
+      coach: user.user.coach?.name,
+      ...user.responses,
+    };
+  });
 
   const columnGroupingModel = data.flatMap((cohort: any) =>
     cohort.forms.map((form: any) => ({
