7642aea9f1bf4c0d4c019245474bc4f26e27a306
David Gusenga <gsngdavid@gmail.com>
2025-01-09 14:02:24 +0200

    refactor: refine the loader

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import classNames from "classnames";
 
-const Loader = () => {
+interface LoaderProps {
+  size?: "xs" | "sm" | "md" | "lg";
+  borderColor?: string;
+}
+
+export default function Loader({
+  size = "md",
+  borderColor = "#0077B6",
+}: LoaderProps) {
   return (
     <div
-      className="border-primary-dark inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      className={classNames(
+        "inline-block animate-spin rounded-full border-solid border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]",
+        {
+          "h-3 w-3 border-2": size === "xs",
+          "h-4 w-4 border-2": size === "sm",
+          "h-8 w-8 border-4": size === "md",
+          "h-12 w-12 border-5": size === "lg",
+        },
+        { [`border-[${borderColor}]`]: borderColor },
+      )}
       role="status"
-    >
-      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-        Loading...
-      </span>
-    </div>
+    ></div>
   );
-};
-
-export default Loader;
+}
