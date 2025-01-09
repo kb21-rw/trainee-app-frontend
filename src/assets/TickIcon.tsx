8e8e89e90f5c69a00824d745cc6b6fd95f10141f
David Gusenga <gsngdavid@gmail.com>
2025-01-09 13:18:39 +0200

    feat: add tick icon

---
+++ src/assets/TickIcon.tsx
@@ -0,0 +1,18 @@
+export default function TickIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      fill="currentColor"
+      height="800px"
+      width="800px"
+      version="1.1"
+      id="Capa_1"
+      viewBox="0 0 490 490"
+      xmlSpace="preserve"
+      className={className}
+    >
+      <polygon points="452.253,28.326 197.831,394.674 29.044,256.875 0,292.469 207.253,461.674 490,54.528 " />
+    </svg>
+  );
+}
