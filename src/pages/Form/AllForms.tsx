72cdd2da032c7508b4f157c2289ff71d5d2dbde9
Victor Mugisha <victormugishavm6@gmail.com>
2025-03-03 15:34:59 +0200

    chore: display the cohort dropdown on All Forms page

--- src/pages/Form/AllForms.tsx
+++ src/pages/Form/AllForms.tsx
@@ -1,25 +1,53 @@
 import React, { useState } from "react"
 import SearchInput from "../../components/ui/SearchInput"
 import {
+  useGetAllCohortsQuery,
   useGetAllFormsQuery,
   useGetApplicationFormQuery,
 } from "../../features/user/backendApi"
 import FormCard from "../../components/ui/FormCard"
-import { Cookie, IFormType } from "../../utils/types"
+import { Cookie, IFormType, AlertType, Cohort } from "../../utils/types"
 import NotFound from "../../components/ui/NotFound"
 import CreateFormDropdown from "../../components/ui/CreateFormDropdown"
+import Loader from "../../components/ui/Loader"
 import { Link } from "react-router-dom"
 import FormsSkeleton from "./FormsSkeleton"
 import { useCookies } from "react-cookie"
+import { getErrorInfo } from "../../utils/helper"
+import { handleShowAlert } from "../../utils/handleShowAlert"
+import { useDispatch } from "react-redux"
+import { FormControl, MenuItem, Select, SelectChangeEvent } from "@mui/material"
 
 const AllForms = () => {
   const [searchQuery, setSearchQuery] = useState("")
+  const dispatch = useDispatch()
   const [cookies] = useCookies([Cookie.jwt])
   const { data, isFetching } = useGetAllFormsQuery({
     jwt: cookies.jwt,
     searchString: searchQuery,
   })
   const { data: applicationForm } = useGetApplicationFormQuery(cookies.jwt)
+
+  const [selectedCohortId, setSelectedCohortId] = useState<string | null>(null)
+  const handleCohortChange = (event: SelectChangeEvent) => {
+    setSelectedCohortId(event.target.value)
+  }
+
+  const {
+    data: cohorts,
+    error: cohortsError,
+    isFetching: cohortsAreFetching,
+  } = useGetAllCohortsQuery({
+    jwt: cookies.jwt,
+  })
+
+  if (cohortsError) {
+    const { message } = getErrorInfo(cohortsError)
+    handleShowAlert(dispatch, {
+      type: AlertType.Error,
+      message,
+    })
+  }
 
   const parsedApplicationForm = {
     _id: applicationForm?._id,
@@ -33,6 +61,34 @@ const AllForms = () => {
 
   return (
     <div className="py-12">
+      <div className="my-10 space-y-10">
+        {cohortsAreFetching ? <Loader /> : null}
+        <div className="flex justify-between items-center">
+          <div className="w-52">
+            <FormControl fullWidth>
+              <Select
+                labelId="cohort-label"
+                id="single-select"
+                defaultValue="Default"
+                value={
+                  selectedCohortId ??
+                  (cohorts
+                    ? cohorts.find((cohort: Cohort) => cohort.isActive)?._id
+                    : "") ??
+                  ""
+                }
+                onChange={handleCohortChange}
+              >
+                {cohorts?.map((cohort: Cohort) => (
+                  <MenuItem key={cohort._id} value={cohort._id}>
+                    {cohort.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+      </div>
       {!isFetching && (
         <div className="flex justify-between items-center my-5">
           <SearchInput setSearchQuery={setSearchQuery} />
