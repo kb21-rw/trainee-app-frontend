9ee3c2f83d07f9bb00d0ad4375d35181467460bb
Rebakure <kevinrebakure@gmail.com>
2025-03-05 17:32:08 +0200

    feat: submit the search form by clicking the button

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -10,18 +10,19 @@ const SearchInput = ({
   const { register, handleSubmit } = useForm()
 
   return (
-    <div className="flex w-full items-center max-w-xl h-14 border border-gray-250 rounded-xl">
-      <div className="bg-primary-dark h-full rounded-tl-xl rounded-bl-xl flex items-center justify-center p-2 text-white space-x-1">
+    <form
+      onSubmit={handleSubmit((data) => setSearchQuery(data.search))}
+      className="border border-gray-250 flex h-12 rounded-lg"
+    >
+      <input
+        className="px-2 outline-none border-none h-full w-full rounded-xl"
+        placeholder="Enter name"
+        {...register("search")}
+      />
+      <button className="bg-primary-dark flex items-center justify-center w-16 rounded-tr-lg rounded-br-lg">
         <SearchIcon />
-      </div>
-      <form onSubmit={handleSubmit((data) => setSearchQuery(data.search))}>
-        <input
-          className="px-2 flex-1 outline-none border-none h-full w-5/6  rounded-xl"
-          placeholder="Enter name"
-          {...register("search")}
-        />
-      </form>
-    </div>
+      </button>
+    </form>
   )
 }
 
