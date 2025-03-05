32986581afbee729eb445e244dd52e663b438977
Rebakure <kevinrebakure@gmail.com>
2025-03-05 09:46:40 +0200

    fix: search input cutting off prematurely

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -1,25 +1,26 @@
-import React, { useRef } from "react"
+import { useForm } from "react-hook-form"
 import SearchIcon from "../../assets/SearchIcon"
 
-const SearchInput = ({ setSearchQuery }: { setSearchQuery: any }) => {
-  const searchRef = useRef<HTMLInputElement>(null)
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value)
-  }
+const SearchInput = ({
+  setSearchQuery,
+}: {
+  // eslint-disable-next-line no-unused-vars
+  setSearchQuery: (data: string) => void
+}) => {
+  const { register, handleSubmit } = useForm()
 
   return (
     <div className="flex w-full items-center max-w-xl h-14 border border-gray-250 rounded-xl">
       <div className="bg-primary-dark h-full rounded-tl-xl rounded-bl-xl flex items-center justify-center p-2 text-white space-x-1">
         <SearchIcon />
       </div>
-      <input
-        className="px-2 flex-1 outline-none border-none h-full w-5/6  rounded-xl"
-        placeholder="Enter name"
-        name="search"
-        onChange={handleInputChange}
-        ref={searchRef}
-      />
+      <form onSubmit={handleSubmit((data) => setSearchQuery(data.search))}>
+        <input
+          className="px-2 flex-1 outline-none border-none h-full w-5/6  rounded-xl"
+          placeholder="Enter name"
+          {...register("search")}
+        />
+      </form>
     </div>
   )
 }
