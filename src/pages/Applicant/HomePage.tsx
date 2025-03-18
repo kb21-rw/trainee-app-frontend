7ffb1af116ba162b2967bf1d7ce612370f3eeb2d
Rebakure <kevinrebakure@gmail.com>
2025-03-18 20:54:27 +0200

    refactor: remove unnecessary socketio initialisations

--- src/pages/Applicant/HomePage.tsx
+++ src/pages/Applicant/HomePage.tsx
@@ -1,13 +1,10 @@
-import { Cookie } from "../../utils/types"
-import { useGetMyApplicationQuery } from "../../features/user/backendApi"
-import Loader from "../../components/ui/Loader"
-import { useSelector } from "react-redux"
-import { RootState } from "../../store"
 import { useCookies } from "react-cookie"
+import { useSelector } from "react-redux"
 import ApplicationFormActions from "../../components/applicationForm/ApplicationFormActions"
-
-import { io } from "socket.io-client"
-import { useEffect } from "react"
+import Loader from "../../components/ui/Loader"
+import { useGetMyApplicationQuery } from "../../features/user/backendApi"
+import { RootState } from "../../store"
+import { Cookie } from "../../utils/types"
 
 export default function HomePage() {
   const role = useSelector((state: RootState) => state.user.role)!
@@ -15,10 +12,6 @@ export default function HomePage() {
   const { data: applicationForm, isLoading } = useGetMyApplicationQuery(
     cookies.jwt,
   )
-
-  useEffect(() => {
-    io("http://localhost:3001/")
-  }, [])
 
   return (
     <div className="flex flex-col items-center justify-center mt-10 md:mt-20 space-y-10">
