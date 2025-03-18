f95c75b0934e8f4fc5f8a00148be2cf28c2663bb
Rebakure <kevinrebakure@gmail.com>
2025-03-18 20:54:00 +0200

    feat: Update the user after they have joined the waitlist

--- src/components/applicationForm/ApplicationFormActions.tsx
+++ src/components/applicationForm/ApplicationFormActions.tsx
@@ -1,15 +1,20 @@
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { getApplicationFormStatus, getFormattedDate } from "../../helper"
+import { WaitListSocketContext } from "../../utils/contexts/WaitListSocketContext"
+import { applicationFormStatusData } from "../../utils/data"
 import {
   ApplicationForm,
   ApplicationFormStatus,
   ButtonSize,
+  Cookie,
   UserResponseQuestion,
   UserRole,
 } from "../../utils/types"
-import { getApplicationFormStatus, getFormattedDate } from "../../helper"
-import { applicationFormStatusData } from "../../utils/data"
-import CohortInfo from "../ui/CohortInfo"
 import Button from "../ui/Button"
+import CohortInfo from "../ui/CohortInfo"
+import { useGetProfileQuery } from "../../features/user/backendApi"
+import { useCookies } from "react-cookie"
 
 interface ApplicationFormActionsProps {
   applicationForm: Omit<ApplicationForm, "questions"> & {
@@ -23,12 +28,32 @@ export default function ApplicationFormActions({
   applicationForm,
   role,
 }: ApplicationFormActionsProps) {
+  const [joinedWaitList, setJoinedWaitList] =
+    useState<ApplicationFormStatus | null>(null)
+
+  const [cookies] = useCookies([Cookie.jwt])
+  const { data } = useGetProfileQuery(cookies.jwt)
   const status =
     role === UserRole.Applicant
       ? ApplicationFormStatus.Submitted
       : getApplicationFormStatus(applicationForm)
 
   const statusBasedData = applicationFormStatusData[status]
+
+  const { socket } = useContext(WaitListSocketContext)
+
+  useEffect(() => {
+    socket?.on("join", (message) => {
+      if (data.email === message.email)
+        setJoinedWaitList(ApplicationFormStatus.JoinedWaitList)
+    })
+
+    socket?.emit("join-room", data.email)
+
+    return () => {
+      socket?.off("join")
+    }
+  }, [socket, data.email])
 
   return (
     <>
@@ -58,20 +83,26 @@ export default function ApplicationFormActions({
         <>
           <div className="text-center flex items-center flex-col">
             <h1 className="text-2xl font-medium text-gray-600 text-center">
-              {statusBasedData.heading}
+              {joinedWaitList
+                ? applicationFormStatusData[joinedWaitList].heading
+                : statusBasedData.heading}
             </h1>
           </div>
           <div className="max-w-2xl mx-auto flex flex-col items-center justify-center border border-gray-300 rounded-lg p-6 shadow-lg space-y-4">
             <p className="text-center text-gray-500">
-              {statusBasedData.description}
+              {joinedWaitList
+                ? applicationFormStatusData[joinedWaitList].description
+                : statusBasedData.description}
             </p>
 
-            <Link
+            <Button
               className="bg-primary-dark text-white px-6 py-3 rounded-md"
-              to={statusBasedData.buttonLink}
+              onClick={() => window.open(statusBasedData.buttonLink, "_blank")}
             >
-              {statusBasedData.buttonText}
-            </Link>
+              {joinedWaitList
+                ? applicationFormStatusData[joinedWaitList].buttonText
+                : statusBasedData.buttonText}
+            </Button>
 
             <div className="text-gray-600 text-sm text-center">
               Learn more about The Gym software developer trainee program{" "}
