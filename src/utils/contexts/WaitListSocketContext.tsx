7a93fbe20a10e34afa0dba5be51d0b31606899f6
Rebakure <kevinrebakure@gmail.com>
2025-03-18 20:55:09 +0200

    feat: create the room

--- src/utils/contexts/WaitListSocketContext.tsx
+++ src/utils/contexts/WaitListSocketContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import { io, Socket } from "socket.io-client"
 
 interface WaitListSocket {
@@ -10,8 +10,6 @@ export const WaitListSocketContext = createContext<WaitListSocket>({
   socket: null,
 })
 
-export const useSocket = () => useContext(WaitListSocketContext)
-
 export function WaitListSocketContextProvider({
   children,
 }: {
@@ -21,7 +19,9 @@ export function WaitListSocketContextProvider({
 
   useEffect(() => {
     const socketInstance = io("http://localhost:3000")
-    socketInstance.on("connect", () => console.log("--- Connected ---"))
+    socketInstance.on("connect", () => {
+      console.log("--- Connected ---")
+    })
     socketInstance.on("disconnect", () => console.log("--- Disconnected ---"))
 
     setSocket({ socket: socketInstance })
