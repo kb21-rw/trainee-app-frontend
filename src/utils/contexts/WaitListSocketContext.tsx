d81200547b04530fb364c9d56d1da830979ad161
Rebakure <kevinrebakure@gmail.com>
2025-03-20 16:17:11 +0200

    feat: add error handling and reconnecting

--- src/utils/contexts/WaitListSocketContext.tsx
+++ src/utils/contexts/WaitListSocketContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useEffect, useState } from "react"
 import { io, Socket } from "socket.io-client"
@@ -15,24 +16,51 @@ export function WaitListSocketContextProvider({
 }: {
   children: React.ReactNode
 }): React.ReactNode {
-  const [socket, setSocket] = useState<WaitListSocket>({ socket: null })
+  const [socket, setSocket] = useState<Socket | null>(null)
+  const socketURL = import.meta.env.VITE_API_URL
 
   useEffect(() => {
-    const socketInstance = io("http://localhost:3000")
-    socketInstance.on("connect", () => {
-      console.log("--- Connected ---")
+    const socketInstance = io(socketURL, {
+      reconnectionAttempts: 3,
+      reconnectionDelay: 1000,
+      timeout: 10000,
     })
-    socketInstance.on("disconnect", () => console.log("--- Disconnected ---"))
 
-    setSocket({ socket: socketInstance })
+    socketInstance.on("connect", () => {
+      console.log("--- Socket Connected ---")
+    })
+
+    socketInstance.on("connect_error", (err) => {
+      console.log(`--- Socket connection error: ${err} ---`)
+    })
+
+    socketInstance.on("reconnect", (attemptNumber) => {
+      console.log(`--- Socket reconnected after ${attemptNumber} attempts ---`)
+    })
+
+    socketInstance.on("reconnect_error", (error) => {
+      console.error("--- Reconnection error:", error.message)
+    })
+
+    socketInstance.on("reconnect_failed", () => {
+      console.error("--- Failed to reconnect ---")
+    })
+
+    socketInstance.io.on("error", (error) => {
+      console.error("--- Socket.io error:", error)
+    })
+
+    setSocket(socketInstance)
 
     return () => {
-      socketInstance.disconnect()
+      if (socketInstance) {
+        socketInstance.disconnect()
+      }
     }
-  }, [])
+  }, [socketURL])
 
   return (
-    <WaitListSocketContext.Provider value={socket}>
+    <WaitListSocketContext.Provider value={{ socket }}>
       {children}
     </WaitListSocketContext.Provider>
   )
