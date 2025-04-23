c08030c34323fef59c909c97701214a524c0585c
Rebakure <kevinrebakure@gmail.com>
2025-04-23 13:29:03 +0200

    feat: add auto focus on form title to represent editing mode

--- src/components/ui/EditableFormCard.tsx
+++ src/components/ui/EditableFormCard.tsx
@@ -179,6 +179,7 @@ export default function EditableFormCard({
           }`}
           {...register("name")}
           disabled={readonly}
+          autoFocus
         />
         <input
           placeholder="Enter description"
