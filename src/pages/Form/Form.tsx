5cd5a3ddbf79ca60469f010e663cfed1b35f430d
AL2002MI08 <alexandramizero@gmail.com>
2025-04-29 10:28:34 +0200

    refactor: use arrow instead of back to form list button

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,6 +1,6 @@
 import { useGetFormQuery } from "../../features/user/backendApi"
 import Loader from "../../components/ui/Loader"
-import { useNavigate, useParams, useSearchParams } from "react-router-dom"
+import { Link, useParams, useSearchParams } from "react-router-dom"
 import EditableFormCard from "../../components/ui/EditableFormCard"
 import QuestionCard from "../../components/ui/QuestionCard"
 import { AlertType, Cookie, TemplateQuestion } from "../../utils/types"
@@ -8,14 +8,13 @@ import { useCookies } from "react-cookie"
 import { getErrorInfo } from "../../utils/helper"
 import { handleShowAlert } from "../../utils/handleShowAlert"
 import { useDispatch } from "react-redux"
-import Button from "../../components/ui/Button"
+import BackIcon from "../../assets/BackIcon"
 
 export default function Form() {
   const [searchParams] = useSearchParams()
   const isEditMode = searchParams.get("edit") === "true"
   const [cookies] = useCookies([Cookie.jwt])
   const dispatch = useDispatch()
-  const navigate = useNavigate()
   const { id } = useParams<{ id: string }>()
   const {
     data: form,
@@ -45,11 +44,13 @@ export default function Form() {
   }
 
   return (
-    <div className="max-w-5xl py-12 mx-auto">
-      <div className="pb-10">
-        <Button onClick={() => navigate(-1)}>Back to form list</Button>
+    <div className="max-w-5xl py-12 mx-auto lg:grid-cols-12 lg:grid">
+      <div className="lg:col-span-1">
+        <Link to=".." relative="path">
+          <BackIcon />
+        </Link>
       </div>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:col-span-11">
         <EditableFormCard form={formProps} readonly={!isEditMode} />
         <div className="flex flex-col gap-4">
           {questions.map((question: TemplateQuestion) => (
