6d4a42ec37eef9bf7c56ee63cea5434b2b0de937
InshutiSouvede <isouvede2@gmail.com>
2025-05-13 09:46:12 +0200

    fix: remove the extended row when the colums are few

--- src/components/ui/OverViewTable.tsx
+++ src/components/ui/OverViewTable.tsx
@@ -29,6 +29,7 @@ import SettingsIcon from "../../assets/SettingsIcon"
 import SettingsModal from "../modals/Settings"
 import EditParticipantModal from "../modals/EditParticipantModal"
 
+
 interface Response extends BaseResponse {
   questionId: string
 }
@@ -79,7 +80,8 @@ export default function OverViewTable({
     form.questions.map(({ _id, prompt, options, required, type }) => ({
       field: _id,
       headerName: prompt,
-      width: 250,
+      flex: 1,
+      minWidth: 200,
       question: { _id, prompt, options, required, type, form: form.name },
       valueFormatter: (value) => value ?? "No response",
     })),
@@ -89,7 +91,8 @@ export default function OverViewTable({
     {
       field: "name",
       headerName: "Name",
-      width: 200,
+      flex: 1,
+      minWidth: 200,
       renderCell: ({ row }) => (
         <div className="flex items-center justify-between">
           <span>{row.name}</span>
@@ -114,8 +117,9 @@ export default function OverViewTable({
     },
     {
       field: "coach",
+      flex: 1,
       headerName: "Coach",
-      width: 200,
+      minWidth: 200,
       editable: true,
       type: "singleSelect",
       valueOptions: [
@@ -123,12 +127,13 @@ export default function OverViewTable({
         ...coaches.map((coach) => ({ value: coach._id, label: coach.name })),
       ],
     },
-    { field: "stage", headerName: "Stage", width: 200 },
+    { field: "stage", flex: 1, headerName: "Stage", minWidth: 200 },
     ...questionColumns,
     {
       field: "actions",
+      flex: 1,
       headerName: "Actions",
-      width: 300,
+      minWidth: 300,
       align: "center",
       type: "singleSelect",
       valueOptions: [
@@ -311,6 +316,8 @@ export default function OverViewTable({
         hideFooter={true}
         onCellClick={handleCellClick}
         disableRowSelectionOnClick
+        autoPageSize
+
         slots={{
           noRowsOverlay: () => (
             <div className="flex items-center justify-center h-full">
