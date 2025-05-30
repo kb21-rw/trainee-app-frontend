bc3f74424ea620050c9169223e83e923466bf360
AL2002MI08 <alexandramizero@gmail.com>
2025-05-30 11:40:20 +0200

    refactor: revert unnecessary changes in the non-protected layout

--- src/components/layouts/NonProtectedLayout.tsx
+++ src/components/layouts/NonProtectedLayout.tsx
@@ -1,30 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { getRoleBasedHomepageURL } from "../../utils/helper"
 import { useCookies } from "react-cookie"
-import { useGetProfileQuery } from "../../features/user/backendApi"
 import { Cookie } from "../../utils/types"
-import Loader from "../ui/Loader"
+
+import { RootState } from "../../store"
+import { useSelector } from "react-redux"
 
 export default function NonProtectLayout() {
   const [cookies] = useCookies([Cookie.jwt])
-  const hasToken = !!cookies.jwt
+  const loggedInUser = useSelector((state: RootState) => state.user)
 
-  const { data: user, isLoading } = useGetProfileQuery(cookies.jwt, {
-    skip: !hasToken,
-  })
-
-  console.log("User logged in", user)
-
-  if (hasToken && user) {
-    return <Navigate to={getRoleBasedHomepageURL(user.role)} />
-  }
-
-  if (hasToken && isLoading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader />
-      </div>
-    )
+  if (cookies.jwt && loggedInUser.role) {
+    return <Navigate to={getRoleBasedHomepageURL(loggedInUser.role)} />
   }
 
   return <Outlet />
