fc4098fc00e1b04a3a58c35c639f98242086a995
Rebakure <kevinrebakure@gmail.com>
2025-07-11 15:05:02 +0200

    feat: create table

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ import { SocketContextProvider } from "./utils/contexts/SocketContext"
 import Trainees from "./pages/Overview/Trainees"
 import CoachGeneralOverview from "./pages/Overview/CoachGeneralOverview"
 import TraineeResults from "./pages/User/TraineeResults"
+import MyApplicants from "./pages/MyApplicants/MyApplicants"
 
 export default function App() {
   const router = createBrowserRouter(
@@ -57,7 +58,7 @@ export default function App() {
             <Route element={<PrivateRoute allowedRoles={[UserRole.Coach]} />}>
               <Route path="/overview" element={<CoachGeneralOverview />} />
               <Route path="/my-trainees" element={<h1>My trainees</h1>} />
-              <Route path="/my-applicants" element={<h1>My applicants</h1>} />
+              <Route path="/my-applicants" element={<MyApplicants />} />
               <Route path="/trainees-results" element={<TraineeResults />} />
             </Route>
             <Route
---
+++ src/pages/MyApplicants/MyApplicants.tsx
@@ -0,0 +1,174 @@
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
+import { useForm } from "react-hook-form"
+import { useDispatch } from "react-redux"
+import DecisionModal from "../../components/modals/DecisionModal"
+import ResponseModal from "../../components/modals/ResponseModal"
+import Loader from "../../components/ui/Loader"
+import NotFound from "../../components/ui/NotFound"
+import OverViewTable from "../../components/ui/OverViewTable"
+import {
+  useApplicantDecisionMutation,
+  useGetApplicantsQuery,
+  useUpdateParticipantMutation,
+} from "../../features/user/backendApi"
+import { handleShowAlert } from "../../utils/handleShowAlert"
+import { getErrorInfo } from "../../utils/helper"
+import {
+  AlertType,
+  DecisionInfo,
+  ResponseModalQuestion,
+  UserRole,
+} from "../../utils/types"
+
+const MyApplicants = () => {
+  const [decisionInfo, setDecisionInfo] = useState<DecisionInfo | null>(null)
+  const [responseInfo, setResponseInfo] = useState<any | null>(null)
+  const [cookies] = useCookies(["jwt"])
+  const dispatch = useDispatch()
+  const { watch } = useForm<{ cohortId: string }>({
+    defaultValues: { cohortId: "" },
+  })
+
+  const {
+    data: cohortOverview,
+    error: cohortOverviewError,
+    isFetching: cohortOverviewIsFetching,
+  } = useGetApplicantsQuery({
+    jwt: cookies.jwt,
+    cohortId: null, // For coach, we don't need to filter by cohort
+  })
+
+  const [
+    decide,
+    {
+      error: decisionError,
+      isSuccess: decidingIsSuccess,
+      reset: applicantDecisionReset,
+    },
+  ] = useApplicantDecisionMutation()
+
+  const [
+    // updateParticipant,
+    {
+      isSuccess: updateParticipantIsSuccess,
+      error: updateParticipantError,
+      reset: updateParticipantReset,
+    },
+  ] = useUpdateParticipantMutation()
+
+  useEffect(() => {
+    const subscription = watch(({ cohortId }) => {
+      // For coach, we don't need to filter by cohort
+      return cohortId
+    })
+
+    return () => subscription.unsubscribe()
+  }, [watch])
+
+  const handleDecision = (userData: DecisionInfo) => {
+    setDecisionInfo({ ...userData })
+  }
+
+  const handleCloseModal = () => {
+    setTimeout(() => setResponseInfo(null), 0)
+  }
+
+  const handleUpsertResponse = (data: {
+    userId: string
+    question: ResponseModalQuestion
+  }) => {
+    setResponseInfo(data)
+  }
+
+  const handleSubmitDecision = async ({ feedback }: { feedback: string }) => {
+    if (!decisionInfo) {
+      return
+    }
+
+    await decide({
+      jwt: cookies.jwt,
+      body: {
+        traineeId: decisionInfo.traineeId,
+        decision: decisionInfo.decision,
+        feedback,
+      },
+    })
+  }
+
+  if (cohortOverviewError || decisionError || updateParticipantError) {
+    const { message } = getErrorInfo(
+      cohortOverviewError ?? decisionError ?? updateParticipantError,
+    )
+    handleShowAlert(dispatch, {
+      type: AlertType.Error,
+      message,
+    })
+    if (decisionError) {
+      setDecisionInfo(null)
+      applicantDecisionReset()
+    }
+  }
+
+  if (decidingIsSuccess) {
+    handleShowAlert(dispatch, {
+      type: AlertType.Success,
+      message: `User is successfully ${decisionInfo?.decision.toLowerCase()}`,
+    })
+    setDecisionInfo(null)
+    applicantDecisionReset()
+  }
+
+  if (updateParticipantIsSuccess) {
+    handleShowAlert(dispatch, {
+      type: AlertType.Success,
+      message: "Coach is successfully changed",
+    })
+    updateParticipantReset()
+  }
+
+  console.log("🚀")
+  console.log(cohortOverview.trainees)
+
+  console.log("🍏")
+  console.log(cohortOverview.participantsInfo)
+
+  return (
+    <div className="flex flex-col h-full py-12 space-y-5">
+      <DecisionModal
+        modalType="applicant"
+        decisionInfo={decisionInfo}
+        closeModal={() => setDecisionInfo(null)}
+        onSubmit={handleSubmitDecision}
+      />
+      {responseInfo && (
+        <ResponseModal
+          responseInfo={responseInfo}
+          closeModal={handleCloseModal}
+        />
+      )}
+
+      {cohortOverviewIsFetching && <Loader />}
+      {cohortOverview && (
+        <OverViewTable
+          role={UserRole.Coach}
+          overviewType="applicant"
+          forms={cohortOverview.forms}
+          participants={cohortOverview.trainees}
+          participantsInfo={cohortOverview.participantsInfo}
+          coaches={cohortOverview.coaches}
+          updates={[]}
+          stages={cohortOverview.stages}
+          actions={{ handleDecision, handleUpsertResponse }}
+        />
+      )}
+      {!cohortOverviewIsFetching && !cohortOverview && (
+        <div className="flex-1">
+          <NotFound entity="User" type="NoData" />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MyApplicants
